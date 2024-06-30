fdafc48278592d00b36c34566314c84b4a13ed5a
Marta Bakešová <marta.bakesova@centrum.cz>
2024-06-30 22:41:56 +0200

    vytvoreni komponenty Layer, docasne props v komponente Drink

---
+++ src/components/Layer/layer.jsx
@@ -0,0 +1,13 @@
+import "./layer.css";
+
+export const Layer = ({ color, label }) => {
+  return (
+    <div className="layer">
+      <div
+        className="layer__color"
+        style={{ backgroundColor: `${color}` }}
+      ></div>
+      <div className="layer__label">{label}</div>
+    </div>
+  );
+};
