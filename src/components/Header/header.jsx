3e12779ca9f02b1f9f66c51d4986ff8309abbe5e
Marta Bakešová <marta.bakesova@centrum.cz>
2024-07-07 16:40:00 +0200

    Header upraveno tak, aby zobrazovalo odkaz na Hlavni Stranku ze stranky objednavky.

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,18 +1,24 @@
 import "./header.css";
 
 export const Header = (props) => {
-  const navigaceClass = props.showMenu ? "navigation" : "navigation nav-hide";
+  const navigaceClass = props.showMenu
+    ? "rollout-nav"
+    : "rollout-nav menu-hide";
+  const inlineNavClass = props.showMenu ? "inline-nav menu-hide" : "inline-nav";
   return (
     <header id="home">
       <div className="header__content container">
         <div className="site-logo"></div>
-        <div className={navigaceClass}>
+        <div className="navigation">
           <button className="nav-btn"></button>
-          <nav className="rollout-nav nav-closed">
+          <nav className={navigaceClass}>
             <a href="#home">domů</a>
             <a href="#menu">menu</a>
             <a href="#gallery">galerie</a>
             <a href="#contact">kontakt</a>
+          </nav>
+          <nav className={inlineNavClass}>
+            <a href="/">Hlavní stránka</a>
           </nav>
         </div>
       </div>
